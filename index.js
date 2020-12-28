90e74781b3a8413d92aadbe791ac28dad321dcd9
Tykky <40919314+Tykky@users.noreply.github.com>
2020-12-28 05:09:19 +0200

    3.8 done

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
 
+morgan.token('content', request => 
+    JSON.stringify(request.body)
+)
+
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :content'))
 
 let notes = [
       {
